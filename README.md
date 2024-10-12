1a4ccc4faaa5bae8489241fe03d41888863a02a5
Agustin Briozzo <agustinbriozzo1997@gmail.com>
2024-10-11 21:02:29 -0300

    migro proyecto original GuitarLA a TypeScript

---
+++ README.md
@@ -0,0 +1,25 @@
+# Tienda de Guitarras 🎸
+
+Esta es una aplicación de tienda de guitarras creada con Vite + React + TypeScript. Permite a los usuarios explorar una variedad de guitarras, ver detalles de cada una y agregarlas a un carrito de compras. El carrito es persistente gracias a `localStorage`, lo que significa que los elementos no se pierden incluso si se recarga la página o se cierra el navegador.
+
+## Demo
+
+[Link a la demo en Netlify](https://abguitars-ts.netlify.app/)
+
+## Características
+
+- **Visualización de productos**: Muestra una lista de guitarras con su nombre, precio y descripción.
+- **Carrito de compras**:
+  - Agregar guitarras al carrito.
+  - Incrementar o decrementar la cantidad de cada guitarra en el carrito.
+  - Eliminar guitarras específicas o vaciar todo el carrito.
+  - Persistencia del carrito utilizando `localStorage`.
+- **Diseño responsivo**: Adaptable a diferentes tamaños de pantalla.
+
+## Tecnologías utilizadas
+
+- **Vite**: Herramienta de construcción rápida para desarrollo.
+- **React**: Biblioteca de JavaScript para la construcción de interfaces de usuario.
+- **TypeScript**
+- **CSS**: Estilizado de la aplicación.
+- **LocalStorage**: Para mantener la persistencia del carrito de compras.
---
+++ src/hooks/useCart.ts
@@ -0,0 +1,88 @@
+import { useState, useEffect, useMemo } from "react";
+import { db } from "../data/db";
+import type { Guitar, CartItem } from "../types";
+
+export const useCart = () => {
+  const initialCart = (): CartItem[] => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
+
+  const MIN_ITEMS = 1;
+  const MAX_ITEMS = 5;
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  function addToCart(item: Guitar) {
+    const itemExist = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExist >= 0) {
+      if (cart[itemExist].quantity >= MAX_ITEMS) return;
+      // si existe en el carrito, encuentro la posicion e incremento la cantidad
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+    } else {
+      // si no existe en el carrito, agrego cantidad = 1
+      const newItem: CartItem = { ...item, quantity: 1 };
+      setCart([...cart, newItem]);
+    }
+  }
+
+  function removeFromCart(id: Guitar["id"]) {
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
+  }
+
+  function decreaseQuantity(id: Guitar["id"]) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function increaseQuantity(id: Guitar["id"]) {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1,
+        };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
+
+  // State Derivado
+  const isEmpty = useMemo(() => cart.length === 0, [cart]);
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity * item.price, 0),
+    [cart]
+  );
+
+  return {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    decreaseQuantity,
+    increaseQuantity,
+    clearCart,
+    isEmpty,
+    cartTotal,
+  };
+};
